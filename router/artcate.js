672b96b453bf6922ac2492a2c8634450225fb9bb
WayneLee7 <waynelee735@hotmail.com>
2022-09-11 14:37:56 +0800

    根据id删改查接口

--- router/artcate.js
+++ router/artcate.js
@@ -2,7 +2,15 @@ const express = require('express')
 const router = express.Router()
 const artcate_handler = require('../router_handler/artcate')
 const expressJoi = require('@escook/express-joi')
-const { add_cate_schema } = require('../schema/artcate')
+const { add_cate_schema, delete_cate_schema, get_cate_schema, update_cate_schema } = require('../schema/artcate')
 router.get('/cates', artcate_handler.getArticleCates)
 router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.addArticleCates)
+
+// 删除文章分类
+router.get('/deletecate/:id', expressJoi(delete_cate_schema), artcate_handler.deleteCateById)
+
+// 根据id获取文章分类
+router.get('/cates/:id', expressJoi(get_cate_schema), artcate_handler.getArtCateById)
+// 根据id更新文章分类数据
+router.post('/updatecate', expressJoi(update_cate_schema), artcate_handler.updateCateById)
 module.exports = router
--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -1,3 +1,4 @@
+const { result } = require('@hapi/joi/lib/base')
 const db = require('../db/index')
 
 exports.getArticleCates = (req, res) => {
@@ -17,11 +18,11 @@ exports.addArticleCates = (req, res) => {
     db.query(sql, [req.body.name, req.body.alias], (err, results) => {
         if (err) return res.cc(err)
         if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试')
-        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.bofy.alias)
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias)
             return res.cc('分类名称与别名被占用，请更换后重试')
         if (results.length === 1 && results[0].name === req.body.name)
             return res.cc('分类名称被占用，请更换后重试')
-        if (results.length === 1 && results[0].alias === req.bofy.alias)
+        if (results.length === 1 && results[0].alias === req.body.alias)
             return res.cc('分类别名被占用，请更换后重试')
     })
     const sql1 = `insert into ev_article_cate set ?`
@@ -29,5 +30,48 @@ exports.addArticleCates = (req, res) => {
         if (err) return res.cc(err)
         if (results.affectedRows !== 1) return res.cc('新增文章分类失败')
         res.cc('新增文章分类成功', 0)
+    })
+}
+
+
+exports.deleteCateById = (req, res) => {
+    const sql = `update ev_article_cate set is_delete=1 where id=?`
+    db.query(sql, req.params.id, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.affectedRows !== 1) return res.cc('删除文章分类失败')
+        return res.cc('删除文章分类成功', 0)
+    })
+}
+
+exports.getArtCateById = (req, res) => {
+    const sql = `select * from ev_article_cate where id=?`
+    db.query(sql, req.params.id, (err, results) => {
+        if (err) return res.cc(err)
+        if (results.length !== 1) return res.cc('获取文章分类数据失败')
+        res.send({
+            status: 0,
+            message: '获取成功',
+            data: results[0]
+        })
+    })
+}
+
+exports.updateCateById = (req, res) => {
+    const sql = `select * from ev_article_cate where id<>? and(name=? or alias=?)`
+    db.query(sql, [req.body.id, req.body.name, req.body.alias], (err, results) => {
+        if (err) return res.cc(err)
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试')
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias)
+            return res.cc('分类名称与别名被占用，请更换后重试')
+        if (results.length === 1 && results[0].name === req.body.name)
+            return res.cc('分类名称被占用，请更换后重试')
+        if (results.length === 1 && results[0].alias === req.body.alias)
+            return res.cc('分类别名被占用，请更换后重试')
+        const sql1 = `update ev_article_cate set ? where id=?`
+        db.query(sql1, [req.body, req.body.id], (err, results) => {
+            if (err) return res.cc(err)
+            if (results.affectedRows !== 1) return res.cc('更新文章分类数据失败')
+            res.cc('更新分类成功', 0)
+        })
     })
 }
